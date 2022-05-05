a3c2de8ad8dd9f065f69467e8705fd66aa6a0988
Wilson Soetomo <wilsonsoetomo@wilsons-mbp.lan>
2022-05-05 16:24:34 -0700

    functional website

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+var cityList = document.querySelector(".cityHistory");
 var APIkey = "3d6e43d79aa3e316dee7cb1fc394f4b1";
 var cities = JSON.parse(localStorage.getItem("city")) || [];
 var APIcity = "https://api.openweathermap.org/data/2.5/weather";
 var APIcord = "http://api.openweathermap.org/data/2.5/onecall";
 let submitBtn = document.querySelector(".searchBarBtn");
 var inputEl = document.querySelector(".cityHere");
+var currentHour = new Date();
+var fiveDaysDiv = $(".fiveDays");
 // GIVEN a weather dashboard with form inputs
 
+function appendCityList(city, i) {
+  $(".cityHistory").append(
+    `<button data-city="${city}" class="cityButton" id="city-${i}"><span class="cityName">${city}</span></button>`
+  );
+  console.log(city);
+  console.log(i);
+}
 //function to take in a city and does a API search, maybe also appends results on page with dynamic html
 function getCity(city) {
   console.log("WILSONS CITY --- ", city);
@@ -15,59 +25,84 @@ function getCity(city) {
       console.log(cityData);
 
       //adding searched city to the list, saving in localStorage
-      cities.push(city);
-      localStorage.setItem("city", JSON.stringify(cities));
-      makeCityList();
+      if (!cities.includes(city)) {
+        cities.push(city);
+        localStorage.setItem("city", JSON.stringify(cities));
+        makeCityList();
+      }
       var lat = cityData.coord.lat;
       var lon = cityData.coord.lon;
       fetch(APIcord + `?lat=${lat}&lon=${lon}&appid=${APIkey}`)
         .then((corRes) => corRes.json())
         .then((corData) => {
+          $(".weatherIcon").attr(
+            "src",
+            `http://openweathermap.org/img/wn/${corData.current.weather[0].icon}.png`
+          );
+          console.log(corData);
           var temp = corData.current.temp;
-          var wind = corData.current.wind;
-          // var humidity = corData.main.humidity;
-          var uv =corData.current.uvi;
-          document.querySelector(".temp").textContent = temp;
-          document.querySelector(".wind").textContent = wind;
-          // document.querySelector(".humidity").textContent = humidity;
-          document.querySelector(".uv").textContent = uv;
+          console.log(temp);
+          var wind = corData.current.wind_speed;
+          console.log(wind);
+          var humidity = corData.current.humidity;
+          console.log(humidity);
+          var uv = corData.current.uvi;
+          console.log(uv);
+          document.querySelector(".cityNameDate").textContent =
+            city + " " + currentHour;
+          document.querySelector(".temp").textContent =
+            parseInt(temp - 273.15) + " Fehrenheit";
+          document.querySelector(".wind").textContent = wind + "MPH";
+          document.querySelector(".humidity").textContent = humidity + "%";
+          document.querySelector(".uv").textContent = uv + " UV index";
+          if (uv < 4) {
+            document.querySelector(".uv").style.backgroundColor = "green";
+          } else if (uv < 7) {
+            document.querySelector(".uv").style.backgroundColor = "yellow";
+          } else {
+            document.querySelector(".uv").style.backgroundColor = "red";
+          }
+          var daily = corData.daily;
+          make5Days(daily);
         });
     });
 }
 //checks for cities stored in localStorage, append them on page if any
+
 function makeCityList() {
-  var cityList = document.querySelector(".cityList");
   cityList.innerHTML = "";
   var cityArr = JSON.parse(localStorage.getItem("city")) || [];
   //loop through cityArr to make buttons
   for (var i = 0; i < cityArr.length; i++) {
-    var cityEl = document.createElement("button");
-    cityEl.textContent = cityArr[i];
-    cityList.append(cityEl);
+    appendCityList(cityArr[i], i);
   }
 }
-
-makeCityList();
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-// WHEN I view current weather conditions for that city
-
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-// WHEN I view the UV index
-
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-// WHEN I view future weather conditions for that city
-
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-// WHEN I click on a city in the search history
-
-// THEN I am again presented with current and future conditions for that city
-
-//create event listener to listen for click on submitBtn
-
 submitBtn.addEventListener("click", function () {
   getCity(inputEl.value.trim());
 });
+function make5Days(daily) {
+  fiveDaysDiv.html("");
+  for (i = 1; i < 6; i++) {
+    console.log(daily);
+    var div = document.createElement("div");
+    var weatherDate = document.createElement("p");
+    var icon = document.createElement("img");
+    var temp = document.createElement("p");
+    var wind = document.createElement("p");
+    var humidity = document.createElement("p");
+
+    weatherDate.textContent = new Date(daily[i].dt * 1000);
+    var iconName = daily[i].weather[0].icon;
+    icon.src = `http://openweathermap.org/img/wn/${iconName}.png`;
+    temp.textContent = "Temp: " + daily[i].temp.day;
+    wind.textContent = "speed = " + daily[i].wind_speed + "MPH";
+    humidity.textContent = "humidity = " + daily[i].humidity + "%";
+
+    div.append(weatherDate);
+    div.append(icon);
+    div.append(temp);
+    div.append(wind);
+    div.append(humidity);
+    fiveDaysDiv.append(div);
+  }
+}
